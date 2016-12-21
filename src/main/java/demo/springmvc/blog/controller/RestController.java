f7dcdc0a04f09ba21640e4b277ead4d05594a14b
miraai <ajla.dzekovic.2470@metropolitan.ac.rs>
2016-12-21 15:32:02 +0100

    Rest start

---
+++ src/main/java/demo/springmvc/blog/controller/RestController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.springmvc.blog.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ *
+ * @author mirai
+ */
+
+@Controller
+@RequestMapping("/post")
+public class RestController {
+    
+}
